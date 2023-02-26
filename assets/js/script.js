e5aa0189e4ffea2b94b73e6a394ab5f4b28b06a8
jmanager25 <jmanager25@hotmail.com>
2023-02-26 20:28:05 +0000

    Add api key and url

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+const API_KEY = "pvYFvu7wq4d2-An1uYm9bgIsIlA";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}; 
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your Key is Valid Until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerHTML = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+
+
+};
